4c5a2a74be64fd5a3873e230f62d34187a0bf44c
cyhex <timor@cyhex.com>
2011-10-18 07:50:40 +0300

    unicode in payload

--- tracker/tests/moodClientServerTest.py
+++ tracker/tests/moodClientServerTest.py
@@ -6,6 +6,9 @@ from tracker.lib.moodClassifierClient import MoodClassifierTCPClient
 
 MCC = MoodClassifierTCPClient('127.0.0.1',6666)
 
-test_data = {'text':'this is a test text'}
+test_data  = [
+{'text':u'this is a test text 1'},
+{'text':u'this is a test text 2'},
+]
 
 print MCC.classify(test_data, 'search')
