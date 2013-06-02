bbb819fb3f23866297c47df692c2b8e135853d95
Timor <timor@cyhex.com>
2013-06-02 09:12:02 +0200

    version merged

--- tracker/collector/trainer/twitterCollector.py
+++ tracker/collector/trainer/twitterCollector.py
@@ -11,19 +11,25 @@ from datetime import datetime
 
 
 class streamCollector(threading.Thread):
-    
+    """ List of keywords to track"""
     trackingK = [':)']
+    
+    """ Limit (how many tweets should be received before quitting ) """
+    limit = 1000000
+    
+    """ Twitter user/pass"""
+    twitterUser = 'your_twitter_username'
+    twitterPass = 'your_twitter_pass'
     
     def __init__(self,tweetsQ,stop_event):
         threading.Thread.__init__(self)
         self.q = tweetsQ
         self.stop_event = stop_event
-        self.limit = 1000000
         self.start_t = time.time()
         self.c = 0
         
     def collect(self):
-        stream = tweetstream.TrackStream("username", "password",keywords=self.trackingK, url='http://stream.twitter.com/1/statuses/filter.json')  
+        stream = tweetstream.TrackStream(self.twitterUser, self.twitterPass ,keywords=self.trackingK, url='https://stream.twitter.com/1/statuses/filter.json')  
         for tweet in stream:
             
             if self.c and self.c % 1000 == 0:
@@ -55,7 +61,9 @@ class streamCollector(threading.Thread):
 
 class streamWriter(threading.Thread):
     
+    """ Destination file """
     fileName = os.path.abspath(os.path.join( os.curdir,os.path.normpath('../../data/tweets_positive_raw.dat')))
+    
     data = []
     
     def __init__(self,tweetsQ,stop_event):
