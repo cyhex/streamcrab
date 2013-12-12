18f040d81c9ba26db86ca0b7be3d5b553aa8e9df
timor <timor@cyhex.com>
2013-12-12 12:45:25 +0100

    fix untitest for logical AND token search

--- tests/test_classifiedStream.py
+++ tests/test_classifiedStream.py
@@ -8,20 +8,27 @@ class TestClassifiedStream(TestCaseDB):
 
     def test_find_tokens(self):
         s1 = ClassifiedStream()
-        s1.tokens = ['a','b']
+        s1.tokens = ['a', 'b', 'c']
         s1.text = 'ab'
         s1.source = 't'
         s1.save()
 
         s2 = ClassifiedStream()
-        s2.tokens = ['b','c']
+        s2.tokens = ['b', 'c']
         s2.text = 'bs'
         s2.source = 't'
         s2.save()
 
-        self.assertEquals(2, len(ClassifiedStream.find_tokens(['a', 'b'])))
+        s3 = ClassifiedStream()
+        s3.tokens = ['b', 'c']
+        s3.text = 'bs'
+        s3.source = 't'
+        s3.save()
+
+        self.assertEquals(1, len(ClassifiedStream.find_tokens(['a', 'b'])))
         self.assertEquals(1, len(ClassifiedStream.find_tokens(['a'])))
-        self.assertEquals(1, len(ClassifiedStream.find_tokens(['a', 'b'], s1.id)))
+        self.assertEquals(1, len(ClassifiedStream.find_tokens(['c', 'b'], s2.id)))
+        self.assertEquals(3, len(ClassifiedStream.find_tokens(['c', 'b'])))
 
     def tearDown(self):
         ClassifiedStream.drop_collection()
